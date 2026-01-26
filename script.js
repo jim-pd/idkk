bfc157caa0cacd1aab31e1d9f904abe4b13c8886
Dimitrios <jimspcs122@gmail.com>
2026-01-26 12:33:52 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+window.onload = function () {
+  let scale = 1;
+
+  const yesBtn = document.getElementById("yes");
+  const noBtn = document.getElementById("no");
+  const questionDiv = document.getElementById("question");
+  const resultDiv = document.getElementById("result");
+
+  const SHEET_URL = "https://script.google.com/macros/s/AKfycbz1zREmzSbmRfcYNqyDA7DvU6uRxN_um1D_NsP3_GFiIMGNljA-aV4PQ74F4Dq0wJUQ/exec";
+
+  function sendAnswer(answer) {
+    fetch(SHEET_URL, {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: "answer=" + answer
+    });
+  }
+
+  noBtn.onclick = function () {
+    sendAnswer("NO");
+    scale += 0.5;
+    yesBtn.style.transform = `scale(${scale})`;
+  };
+
+  yesBtn.onclick = function () {
+    sendAnswer("YES");
+    questionDiv.style.display = "none";
+    resultDiv.style.display = "block";
+  };
+};
